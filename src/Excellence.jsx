c4d546984eb2a166702a90adf225163fc4784daf
AndrewLange1 <lang1395@umn.edu>
2025-06-22 14:33:35 -0500

    Fixing scaling

--- src/Excellence.jsx
+++ src/Excellence.jsx
@@ -17,18 +17,19 @@ function Excellence() {
         </div>
         <div className="med:hidden flex flex-col items-center mt-[4%] text-[clamp(18px,2.8vw,20px)] font-extralight px-[10%] text-center leading-relaxed w-full max-w-[1000px]">
         <span>Here at McGeehan Construction we pride</span>
-        <span>ourselvesourselves in the relationships we</span>
-        <span>build with our customers. Whether you are</span>
-        <span>looking for a small backyard project or a</span>
-        <span>large commercial renovation, we will work with</span>
-        <span>you to make sure your plans go to action asap.</span>
-        <span>With a close-knit local team you can expect</span>
-        <span>fast - and easy - communication. Our services</span>
-        <span>include residential, commercial, and other</span>
-        <span>miscellaneous projects. If you have an idea,</span>
-        <span>we’ll work with you to help get it done! Working</span>
-        <span>with pros has never been easier. Call us today</span>
-        <span>for a free consultation — we’re</span>
+        <span>ourselvesourselves in the relationships</span>
+        <span>we build with our customers. Whether</span>
+        <span>you are looking for a small backyard</span>
+        <span>project or a large commercial renovation,</span>
+        <span>we will work with you to make sure your</span>
+        <span>plans go to action asap. With a close-knit</span>
+        <span>local team you can expect fast - and easy -</span>
+        <span>communication. Our services include</span>
+        <span>residential, commercial, and other miscellaneous</span>
+        <span>projects. If you have an idea, we’ll work with</span>
+        <span>you to help get it done! Working with pros</span>
+        <span>has never been easier. Call us today for</span>
+        <span>a free consultation — we’re</span>
         <span>excited to help!</span>
         </div>
         <a
