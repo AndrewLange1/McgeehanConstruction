23094a82ede3c56cbc32e30452035dece685e470
AndrewLange1 <lang1395@umn.edu>
2025-06-22 14:30:01 -0500

    Still trying stuff out

--- src/Excellence.jsx
+++ src/Excellence.jsx
@@ -4,7 +4,7 @@ import './index.css'
 function Excellence() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center mt-[5%] text-[clamp(20px,3vw,35px)] font-light">
+      <div className="flex flex-col items-center justify-center mt-[5%] text-[clamp(23px,3.5vw,35px)] font-light">
         Excellence – It's what we do
         <div className="med:flex flex-col items-center mt-[5%] text-[clamp(18px,2vw,23px)] font-extralight hidden px-4 text-center leading-relaxed">
           <span>Here at McGeehan Construction we pride ourselves in the relationships we build with our customers.</span>
@@ -15,20 +15,21 @@ function Excellence() {
           <span>professionals has never been easier. Be sure to call us today for a free consultation.</span>
           <span>We’re looking forward to working with you!</span>
         </div>
-        <div className="med:hidden flex flex-col items-center mt-[4%] text-[clamp(17px,2.8vw,20px)] font-extralight px-[10%] text-center leading-relaxed w-full max-w-[1000px]">
+        <div className="med:hidden flex flex-col items-center mt-[4%] text-[clamp(18px,2.8vw,20px)] font-extralight px-[10%] text-center leading-relaxed w-full max-w-[1000px]">
         <span>Here at McGeehan Construction we pride</span>
-        <span>ourselvesourselves in the relationships we build</span>
-        <span>with our customers. Whether you are looking</span>
-        <span>for a small backyard project or a large commercial</span>
-        <span>renovation, we will work with you. To make sure</span>
-        <span>your plans go to action asap. With a close-knit</span>
-        <span>local team you can expect fast - and easy - </span>
-        <span>communication. Our services include residential,</span>
-        <span>commercial, and other miscellaneous projects. If</span>
-        <span>you have an idea, we’ll work with you to help get</span>
-        <span>it done! Working with pros has never been easier.</span>
-        <span>Call us today for a free consultation —</span>
-        <span>we’re excited to help!</span>
+        <span>ourselvesourselves in the relationships we</span>
+        <span>build with our customers. Whether you are</span>
+        <span>looking for a small backyard project or a</span>
+        <span>large commercial renovation, we will work with</span>
+        <span>you to make sure your plans go to action asap.</span>
+        <span>With a close-knit local team you can expect</span>
+        <span>fast - and easy - communication. Our services</span>
+        <span>include residential, commercial, and other</span>
+        <span>miscellaneous projects. If you have an idea,</span>
+        <span>we’ll work with you to help get it done! Working</span>
+        <span>with pros has never been easier. Call us today</span>
+        <span>for a free consultation — we’re</span>
+        <span>excited to help!</span>
         </div>
         <a
           href="mailto:mcgeehanconstruction@gmail.com"
